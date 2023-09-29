10be4625771a29ad06879e213d15606b1711b751
code-wiki <aditya.gautam003@gmail.com>
2023-09-29 18:23:11 +0530

    improving UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,42 @@
-import "bootstrap/dist/css/bootstrap.css";
-import type { Metadata } from "next";
-import dynamic from "next/dynamic";
-import localFont from "next/font/local";
-import "../style/globals.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import type { Metadata } from 'next';
+import dynamic from 'next/dynamic';
+import localFont from 'next/font/local';
+import '../style/globals.css';
 
 const binancePlex = localFont({
-  src: [
-    {
-      path: "../assets/fonts/BinancePlexRegular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/BinancePlex-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/BinancePlex-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-  ],
+    src: [
+        {
+            path: '../assets/fonts/BinancePlexRegular.ttf',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../assets/fonts/BinancePlex-Medium.ttf',
+            weight: '500',
+            style: 'normal',
+        },
+        {
+            path: '../assets/fonts/BinancePlex-SemiBold.ttf',
+            weight: '600',
+            style: 'normal',
+        },
+    ],
 });
 
 export const metadata: Metadata = {
-  title: "Aditya Gautam",
-  description: "Aditya Gautam Portfolio",
+    title: 'Aditya Gautam',
+    description: 'Aditya Gautam Portfolio',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={binancePlex.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={binancePlex.className}>{children}</body>
+        </html>
+    );
 }
--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -1,70 +1,79 @@
-import Image from "next/image";
-import Link from "next/link";
-import { BiLinkExternal } from "react-icons/bi";
+import Image from 'next/image';
+import Link from 'next/link';
+import { BiLinkExternal } from 'react-icons/bi';
 
 export default function Hero() {
-  return (
-    <div className="container h-100 c-main">
-      <div className="row">
-        <div className="col-md-4">
-          <Link className="navbar-brand d-flex align-items-end" href="#">
-            <Image
-              src={"/coder-3.png"}
-              className={"img-cartoon"}
-              alt="image displaying cartoon"
-              width={100}
-              height={100}
-              loading={"eager"}
-            />
-          </Link>
+    return (
+        <div className="container h-100 c-main">
+            <div className="row">
+                <div className="col-md-4">
+                    <Link
+                        className="navbar-brand d-flex align-items-end"
+                        href="#"
+                    >
+                        <Image
+                            src={'/coder-3.png'}
+                            className={'img-cartoon'}
+                            alt="image displaying cartoon"
+                            width={100}
+                            height={100}
+                            loading={'eager'}
+                        />
+                    </Link>
+                </div>
+                <div className="col-md-5 about-me">
+                    <div className="text-regular">
+                        <span className="text-bold">
+                            Hi, I am Aditya Gautam.
+                        </span>
+                        <br />
+                        an experienced and accomplished entrepreneur and speaker
+                        specializing in building technology startups. With a
+                        proven track record of success, I have co-founded
+                        multiple startups that delivered approximately seven
+                        times returns to investors. <br />
+                        This success is a testament to my exceptional strategic
+                        thinking, business acumen, and deep understanding of the
+                        market. <br />
+                        Additionally, I have worked with prestigious
+                        organizations such as Hewlett Packard R&D and the Tezos
+                        India Foundation, demonstrating my diverse range of
+                        skills and extensive experience that I can leverage to
+                        deliver outstanding results.
+                        <br />
+                    </div>
+                    <div className="social-links-container d-flex w-100 mt-4">
+                        <a
+                            href={'https://www.linkedin.com/in/me-adi'}
+                            className="social-link mr-10 pr-4 d-flex align-items-center"
+                            aria-label="linkedin link"
+                        >
+                            {' '}
+                            LinkedIn&nbsp;
+                            <BiLinkExternal />
+                        </a>
+                        <a
+                            href={'https://www.linkedin.com/in/me-adi'}
+                            className="social-link mr-10 pr-4 d-flex align-items-center"
+                            aria-label="Twitter link"
+                        >
+                            {' '}
+                            Twitter&nbsp;
+                            <BiLinkExternal />
+                        </a>
+                        <a
+                            href={'https://www.linkedin.com/in/me-adi'}
+                            className="social-link mr-10 pr-4 d-flex align-items-center"
+                            aria-label="Blog link"
+                        >
+                            {' '}
+                            Blog&nbsp;
+                            <BiLinkExternal />
+                        </a>
+                    </div>
+                </div>
+                <div className="col-md-3"></div>
+            </div>
         </div>
-        <div className="col-md-5 about-me">
-          <div className="text-regular">
-            <span className="text-bold">Hi, I am Aditya Gautam.</span>
-            <br />
-            an experienced and accomplished entrepreneur and speaker
-            specializing in building technology startups. With a proven track
-            record of success, I have co-founded multiple startups that
-            delivered approximately seven times returns to investors. <br />
-            This success is a testament to my exceptional strategic thinking,
-            business acumen, and deep understanding of the market. <br />
-            Additionally, I have worked with prestigious organizations such as
-            Hewlett Packard R&D and the Tezos India Foundation, demonstrating my
-            diverse range of skills and extensive experience that I can leverage
-            to deliver outstanding results.
-          </div>
-          <div className="social-links-container d-flex w-100 ">
-            <a
-              href={"https://www.linkedin.com/in/me-adi"}
-              className="social-link mr-10 pr-4 d-flex align-items-center"
-              aria-label="linkedin link"
-            >
-              {" "}
-              LinkedIn&nbsp;
-              <BiLinkExternal />
-            </a>
-            <a
-              href={"https://www.linkedin.com/in/me-adi"}
-              className="social-link mr-10 pr-4 d-flex align-items-center"
-              aria-label="Twitter link"
-            >
-              {" "}
-              Twitter&nbsp;
-              <BiLinkExternal />
-            </a>
-            <a
-              href={"https://www.linkedin.com/in/me-adi"}
-              className="social-link mr-10 pr-4 d-flex align-items-center"
-              aria-label="Blog link"
-            >
-              {" "}
-              Blog&nbsp;
-              <BiLinkExternal />
-            </a>
-          </div>
-        </div>
-        <div className="col-md-3"></div>
-      </div>
-    </div>
-  );
+    );
 }
